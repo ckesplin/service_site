eb396cdb1647a3803c1445faa539ca0ca6a0cc69
unknown <ckesplin@gmail.com>
2023-04-21 16:16:59 -0600

    Change app header function  - adds sections to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import {ThemeProvider} from "@mui/material";
-import {lightTheme} from "./capitalChimneyTheme";
+import {Box, Card, ThemeProvider, Typography} from "@mui/material";
+import {lightTheme} from "./theme";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
-import './App.css'
+import '/css/App.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <Box className='App'>
       <ThemeProvider theme={lightTheme}>
         <ResponsiveAppBar />
+        <Box display='flex' flexDirection='column' alignItems='center' width='100%' height="40vh">
+          <Typography variant="h1">THE HERO SECTION</Typography>
+          <Typography>Contact Info</Typography>
+        </Box>
+        <Box display='flex' alignItems='center' flexDirection='column'>
+          <Card sx={{width: "80vw", marginBottom: "36px"}}>
+            Contact Card
+          </Card>
+          <Card sx={{width: "80vw", marginBottom: "36px"}}>
+            Services Card
+          </Card>
+          <Card sx={{width: "80vw", marginBottom: "36px"}}>
+            FAQ Card
+          </Card>
+          <Card sx={{width: "80vw", marginBottom: "36px"}}>
+            Contact Card
+          </Card>
+        </Box> 
       </ThemeProvider>
-    </div>
+    </Box>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,23 @@
+import {createTheme} from "@mui/material";
+
+
+export const lightTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#006688',
+    },
+    secondary: {
+      main: '#4e616d',
+    },
+    error: {
+      main: '#ba1a1a',
+    },
+    background: {
+      default: '#fbfcfe',
+    },
+    text: {
+      primary: "#333",
+      secondary: "#fff",
+    }
+  },
+})
